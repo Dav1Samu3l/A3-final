93e2274c7b5e8905a9f64e60c0825e5bfcdab247
Dav1Samu3l <daviflorianopolis@gmail.com>
2025-06-01 18:56:46 -0300

    organiza a estrutura do script SQL para criação do banco de dados e tabelas

--- MySqlScript.sql
+++ MySqlScript.sql
@@ -1,27 +1,35 @@
-CREATE DATABASE  db_estoque;
+        
 
-USE db_estoque;
 
-CREATE TABLE IF NOT EXISTS produto (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    preco_unitario DECIMAL(10, 2) NOT NULL,
-    unidade VARCHAR(20) NOT NULL,
-    quantidade INT NOT NULL,
-    quantidade_minima INT NOT NULL,
-    quantidade_maxima INT NOT NULL,
-    categoria_id INT  
-);
 
-ALTER TABLE produto 
-ADD CONSTRAINT fk_produto_categoria 
-FOREIGN KEY (categoria_id) 
-REFERENCES categoria(id);
+        CREATE DATABASE  db_estoque;
 
-CREATE TABLE  categoria (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL UNIQUE,
-    tamanho ENUM('Pequeno', 'Médio', 'Grande') NOT NULL DEFAULT 'Médio',
-    embalagem ENUM('Lata', 'Plástico', 'Vidro') NOT NULL
-) ENGINE=InnoDB;
+        USE db_estoque;
+
+        CREATE TABLE IF NOT EXISTS produto (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(100) NOT NULL,
+            preco_unitario DECIMAL(10, 2) NOT NULL,
+            unidade VARCHAR(20) NOT NULL,
+            quantidade INT NOT NULL,
+            quantidade_minima INT NOT NULL,
+            quantidade_maxima INT NOT NULL,
+            categoria_id INT  
+        )ENGINE=InnoDB;
+
+        CREATE TABLE  categoria (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(100) NOT NULL,
+            tamanho ENUM('Pequeno', 'Médio', 'Grande') NOT NULL DEFAULT 'Médio',
+            embalagem ENUM('Lata', 'Plástico', 'Vidro') NOT NULL
+        ) ENGINE=InnoDB;
+
+        ALTER TABLE produto 
+        ADD CONSTRAINT fk_produto_categoria 
+        FOREIGN KEY (categoria_id) 
+        REFERENCES categoria(id);
+
+
+
+
 
